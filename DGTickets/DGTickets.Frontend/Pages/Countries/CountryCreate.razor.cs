8e68b9af2051387f58e37229e652d4927c40b8e8
Batman <diegoalejandromona12@gmail.com>
2024-09-22 22:09:55 -0500

    feat: :sparkles: Added visual part for countries entity

---
+++ DGTickets/DGTickets.Frontend/Pages/Countries/CountryCreate.razor.cs
@@ -0,0 +1,39 @@
+using DGTickets.Frontend.Repositories;
+using DGTickets.Shared.Entities;
+using DGTickets.Shared.Resources;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using MudBlazor;
+
+namespace DGTickets.Frontend.Pages.Countries;
+
+public partial class CountryCreate
+{
+    private CountryForm? countryForm;
+    private Country country = new();
+
+    [Inject] private IRepository Repository { get; set; } = null!;
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
+    [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
+
+    private async Task CreateAsync()
+    {
+        var responseHttp = await Repository.PostAsync("/api/countries", country);
+        if (responseHttp.Error)
+        {
+            var message = await responseHttp.GetErrorMessageAsync();
+            Snackbar.Add(Localizer[message!], Severity.Error);
+            return;
+        }
+
+        Return();
+        Snackbar.Add(Localizer["RecordCreatedOk"], Severity.Success);
+    }
+
+    private void Return()
+    {
+        countryForm!.FormPostedSuccessfully = true;
+        NavigationManager.NavigateTo("/countries");
+    }
+}
