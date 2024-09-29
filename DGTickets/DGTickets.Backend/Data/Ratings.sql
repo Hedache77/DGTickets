1694c32c34cbfbc8a3f81937620a5e4c4f1c1a3e
LIZET VANESA MEDINA CUADRADO <vanesitamedina@gmail.com>
2024-09-29 17:17:15 -0500

    fix: :bug: Added new values on Rating seeders

--- DGTickets/DGTickets.Backend/Data/Ratings.sql
+++ DGTickets/DGTickets.Backend/Data/Ratings.sql
@@ -1,3 +1,14 @@
 ﻿SET IDENTITY_INSERT [dbo].[Ratings] ON 
 INSERT [dbo].[Ratings] ([Id], [Value], [Description], [Code]) VALUES (1, 2,N'Bien!',N'F311');
+INSERT [dbo].[Ratings] ([Id], [Value], [Description], [Code]) VALUES (2, 2,N'Bien!',N'F312');
+INSERT [dbo].[Ratings] ([Id], [Value], [Description], [Code]) VALUES (3, 2,N'Bien!',N'F313');
+INSERT [dbo].[Ratings] ([Id], [Value], [Description], [Code]) VALUES (4, 2,N'Bien!',N'F314');
+INSERT [dbo].[Ratings] ([Id], [Value], [Description], [Code]) VALUES (5, 2,N'Bien!',N'F315');
+INSERT [dbo].[Ratings] ([Id], [Value], [Description], [Code]) VALUES (6, 2,N'Bien!',N'F316');
+INSERT [dbo].[Ratings] ([Id], [Value], [Description], [Code]) VALUES (7, 2,N'Bien!',N'F317');
+INSERT [dbo].[Ratings] ([Id], [Value], [Description], [Code]) VALUES (8, 2,N'Bien!',N'F318');
+INSERT [dbo].[Ratings] ([Id], [Value], [Description], [Code]) VALUES (9, 2,N'Bien!',N'F319');
+INSERT [dbo].[Ratings] ([Id], [Value], [Description], [Code]) VALUES (10, 2,N'Bien!',N'F320');
+INSERT [dbo].[Ratings] ([Id], [Value], [Description], [Code]) VALUES (11, 2,N'Bien!',N'F321');
+INSERT [dbo].[Ratings] ([Id], [Value], [Description], [Code]) VALUES (12, 2,N'Bien!',N'F322');
 SET IDENTITY_INSERT [dbo].[Ratings] OFF
