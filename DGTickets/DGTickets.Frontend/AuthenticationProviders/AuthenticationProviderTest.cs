5e7c0d360f9b8bfdf5f16319a9edb7d59475be24
Batman <diegoalejandromona12@gmail.com>
2024-10-02 15:55:45 -0500

    feat: :sparkles: Simplify list

--- DGTickets/DGTickets.Frontend/AuthenticationProviders/AuthenticationProviderTest.cs
+++ DGTickets/DGTickets.Frontend/AuthenticationProviders/AuthenticationProviderTest.cs
@@ -9,13 +9,13 @@ public class AuthenticationProviderTest : AuthenticationStateProvider
     {
         await Task.Delay(3000);
         var user = new ClaimsIdentity(authenticationType: "test");
-        var admin = new ClaimsIdentity(new List<Claim>
-    {
+        var admin = new ClaimsIdentity(
+    [
         new Claim("FirstName", "DG"),
         new Claim("LastName", "Tickets"),
         new Claim(ClaimTypes.Name, "dgtickets@yopmail.com"),
         new Claim(ClaimTypes.Role, "Admin")
-    },
+    ],
         authenticationType: "test");
 
         return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(user)));
